26ce2864c59fb8783571767ac754a17a2cfd0416
Jan Hindges <janfhindges@gmail.com>
2025-07-20 18:37:56 +0200

    [OCaml] Add skip instructions

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -10,12 +10,9 @@ module Instruction = struct
     | Return (** PC = top stack *)
     | Jump of int (** PC = given address *)
     | Call of int (** ++SP = PC, PC = given address *)
-  (*
-     | Skip_if_val_eq of int (** PC += 2 if equal *)
-     | Skip_if_val_neq of int (** PC += 2 if not equal *)
-     | Skip_if_reg_eq of int (** PC += 2 if equal *)
-     | Skip_if_reg_neq of int (** PC += 2 if not equal *)
-  *)
+    | Skip_if_val_eq of int * int (** SE 3xkk: if Vx = kk then PC += 2 *)
+    | Skip_if_val_neq of int * int (** SNE 4xkk: if Vx <> kk then PC += 2 *)
+    | Skip_if_reg_eq of int * int (** SE 5xy0: if Vx = Vy then PC += 2 *)
 end
 
 type config = { pixel_scale : int }
@@ -39,6 +36,7 @@ type memory = char array
 (** *)
 type state =
   { pc : int ref
+  ; registers : int array
   ; stack : int list ref
       (* TODO: Emulates stack until usage within memory block is figured out *)
   ; memory : memory
@@ -49,6 +47,7 @@ type state =
 let setup () : state =
   let screen_len = facts.width * facts.height in
   { pc = ref 0x200
+  ; registers = Array.create ~len:16 0
   ; stack = ref []
   ; memory = Array.create ~len:facts.memory (Char.of_int_exn 0)
   ; screen = Array.create ~len:screen_len facts.background_color
@@ -67,6 +66,17 @@ let decode : Instruction.encoded -> Instruction.decoded = function
   | high, low when Int.bit_and high 0x20 = 0x20 ->
     let address = (Int.bit_and high 0x0F |> Fn.flip Int.shift_left 2) + low in
     Call address
+  | high, low when Int.bit_and high 0x30 = 0x30 ->
+    let register = Int.bit_and high 0x0F in
+    Skip_if_val_eq (register, low)
+  | high, low when Int.bit_and high 0x40 = 0x40 ->
+    let register = Int.bit_and high 0x0F in
+    Skip_if_val_neq (register, low)
+  | high, low when Int.bit_and high 0x50 = 0x50 && Int.bit_and low 0 = 0 ->
+    (* TODO: Is this pattern correct? *)
+    let register1 = Int.bit_and high 0x0F in
+    let register2 = Int.bit_and low 0xF0 in
+    Skip_if_reg_eq (register1, register2)
   | _ -> failwith "unknown instruction"
 ;;
 
@@ -88,6 +98,17 @@ let execute state : Instruction.decoded -> unit = function
   | Call address ->
     state.stack := !(state.pc) :: !(state.stack);
     state.pc := address
+  | Skip_if_val_eq (register, value) ->
+    let increment = if state.registers.(register) = value then 2 else 1 in
+    state.pc := !(state.pc) + increment
+  | Skip_if_val_neq (register, value) ->
+    let increment = if state.registers.(register) <> value then 2 else 1 in
+    state.pc := !(state.pc) + increment
+  | Skip_if_reg_eq (register1, register2) ->
+    let increment =
+      if state.registers.(register1) <> state.registers.(register2) then 2 else 1
+    in
+    state.pc := !(state.pc) + increment
 ;;
 
 (** *)
